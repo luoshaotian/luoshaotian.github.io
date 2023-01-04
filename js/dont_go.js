18ed66abcc397cb0291a3dba6c72524254fda44a
luoshaotian <luojian20011025@gmail.com>
2023-01-04 21:19:59 +0800

    Site updated: 2023-01-04 21:19:59

--- js/dont_go.js
+++ js/dont_go.js
@@ -1,6 +1,14 @@
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState == 'hidden') {
-        normal_title = document.title;
-        document.title = '再看看嘛~别走(／ω＼)';
-    } else document.title = normal_title;
-});
+var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            document.title = '再看看嘛~别走(／ω＼)~' + OriginTitile;
+            clearTimeout(titleTime);
+        }
+        else {
+            document.title = '(≧∇≦)ﾉ欢迎回来~~' + OriginTitile;
+            titleTime = setTimeout(function() {
+                document.title = OriginTitile;
+            }, 4000);
+        }
+    });
